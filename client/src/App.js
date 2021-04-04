30e3705b87993e4b1f4d5602c8012322e72a2857
Alec Mitrokostas <apmitrokostas@gmail.com>
2021-04-03 21:59:04 -0400

    read address from wallet

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,53 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navbar from './Navbar.js';
 import Register from './pages/Register.js';
 import VoterHome from './pages/VoterHome.js';
 import AdminHome from './pages/AdminHome.js';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-        <Navbar />
-        <AdminHome />
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      account: '',
+    }
+  }
+
+  async componentWillMount() {
+    await this.loadWeb3()
+    await this.loadBlockchainData()
+  }
+
+  async loadWeb3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert('Non-Ethereum browser detected')
+    }
+    console.log(this.state.address)
+  }
+
+  async loadBlockchainData() {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Navbar account={this.state.account}/>
+          <AdminHome />
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import { React, Component } from 'react';
 import './Navbar.css';
 
-function Navbar() {
-    return (
-        <div className="navbar-wrapper">
-            <h2 className="title">SCVS Demo</h2>
-            <p className="role">VOTER</p>
-            <p className="address">address placeholder</p>
-        </div>
-    )
+class Navbar extends Component {
+
+    async componentDidMount() {
+        console.log(this.props.account)
+    }
+    render() {
+        return (
+            <div className="navbar-wrapper">
+                <h2 className="title">SCVS Demo</h2>
+                <p className="role">VOTER</p>
+                <p className="address">{this.props.account}</p>
+            </div>
+        )
+    }
 }
 
 export default Navbar;
