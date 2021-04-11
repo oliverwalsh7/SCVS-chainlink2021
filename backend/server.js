8965bc7e4a5d1c4e310fa082859b319c32ec8923
oliverwalsh7 <oliverwalsh7@gmail.com>
2021-04-10 20:18:25 -0400

    LETS GO! express

--- backend/server.js
+++ backend/server.js
@@ -1,37 +1,64 @@
 const http = require('http')
 var pg = require('pg');
-//or native libpq bindings
-//var pg = require('pg').native
-const fetch = require("node-fetch");
+const path = require('path');
+const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+
 var resp = ''
 
-const server = http.createServer();
-const port = 8081
-const host = 'localhost'
-server.listen(port, host)
-console.log(`Listening at http://${host}:${port}`)
+// const server = http.createServer();
+// const port = 8081
+// const host = 'localhost'
+// server.listen(port, host)
+// console.log(`Listening at http://${host}:${port}`)
 
-server.on('request', async (request, response) => {
-  if (request.method == 'POST') {
-    console.log('POST')
-    let URLParams = new URLSearchParams(request.url)
-    query = `UPDATE linktopia SET
-      address='${URLParams.get('address')}' WHERE
-      ssn='${URLParams.get('/?ssn')}' AND
-      first_name='${URLParams.get('fname')}' AND
-      last_name='${URLParams.get('lname')}' AND
-      dob='${URLParams.get('dob')}'`
-    console.log(query)
-    await getDB(query)
-    response.writeHead(200, {
-    "Access-Control-Allow-Headers": "*",
-    "Access-Control-Allow-Origin" : "*",
-    'Content-Type': 'application/json'
-  })
-    response.end(JSON.stringify(resp));
-  } else {
+startExpress = async() => {
+  const app = express()
+
+  var allowedOrigins = ['http://localhost:8081'];
+
+  app.use(cors({
+
+  origin: function(origin, callback){
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+
+  exposedHeaders: ['Access-Control-Allow-Origin'],
+
+  credentials: true,
+}));
+
+  app.get("*", cors({
+
+    origin: function(origin, callback){
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  
+    exposedHeaders: ['Access-Control-Allow-Origin'],
+  
+    credentials: true,
+  }), function(req, res) {
+    console.log(req.headers)
     console.log('GET')
-    let URLParams = new URLSearchParams(request.url)
+    console.log(req.url)
+    let URLParams = new URLSearchParams(req.url)
     let query = '';
     if (URLParams.get('type') == 1) {
       query = `SELECT COUNT(*) from linktopia WHERE
@@ -48,16 +75,99 @@ server.on('request', async (request, response) => {
       query = `SELECT * from linktopia WHERE
       address='${URLParams.get('address')}'`
     }
-    await getDB(query)
-    console.log(resp)
-    response.writeHead(200, {
-      "Access-Control-Allow-Headers": "*",
-      "Access-Control-Allow-Origin" : "*",
-      'Content-Type': 'application/json'
-    })
-    response.end(JSON.stringify(resp));
-  } 
-})
+    getDB(query)
+    console.log(res)
+    res.send(resp)
+  })
+
+  app.post('*', cors({
+
+    origin: function(origin, callback){
+      // allow requests with no origin
+      // (like mobile apps or curl requests)
+      if(!origin) return callback(null, true);
+      if(allowedOrigins.indexOf(origin) === -1){
+        var msg = 'The CORS policy for this site does not ' +
+                  'allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+  
+    exposedHeaders: ['Access-Control-Allow-Origin'],
+  
+    credentials: true,
+  }), function(req, res) {
+    if (request.method == 'POST') {
+      console.log('POST')
+      let URLParams = new URLSearchParams(request.url)
+      query = `UPDATE linktopia SET
+        address='${URLParams.get('address')}' WHERE
+        ssn='${URLParams.get('/?ssn')}' AND
+        first_name='${URLParams.get('fname')}' AND
+        last_name='${URLParams.get('lname')}' AND
+        dob='${URLParams.get('dob')}'`
+      console.log(query)
+      getDB(query)
+      console.log(res)
+      res.send(resp)
+    }})
+
+  const port = 8081
+  app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  })
+
+}
+
+
+// server.on('request', async (request, response) => {
+//   if (request.method == 'POST') {
+//     console.log('POST')
+//     let URLParams = new URLSearchParams(request.url)
+//     query = `UPDATE linktopia SET
+//       address='${URLParams.get('address')}' WHERE
+//       ssn='${URLParams.get('/?ssn')}' AND
+//       first_name='${URLParams.get('fname')}' AND
+//       last_name='${URLParams.get('lname')}' AND
+//       dob='${URLParams.get('dob')}'`
+//     console.log(query)
+//     await getDB(query)
+//     response.writeHead(200, {
+//     "Access-Control-Allow-Headers": "*",
+//     "Access-Control-Allow-Origin" : "*",
+//     'Content-Type': 'application/json'
+//   })
+//     response.end(JSON.stringify(resp));
+//   } else {
+//     console.log('GET')
+//     let URLParams = new URLSearchParams(request.url)
+//     let query = '';
+//     if (URLParams.get('type') == 1) {
+//       query = `SELECT COUNT(*) from linktopia WHERE
+//         ssn='${URLParams.get('/?ssn')}' AND
+//         first_name='${URLParams.get('fname')}' AND
+//         last_name='${URLParams.get('lname')}' AND
+//         dob='${URLParams.get('dob')}' AND
+//         address IS NULL`
+//     } else if (URLParams.get('type') == 2) {
+//       query = `SELECT ssn, first_name, last_name, dob, town, state FROM linktopia 
+//       ORDER BY RANDOM()
+//       LIMIT 1`
+//     } else if (URLParams.get('type') == 3) {
+//       query = `SELECT * from linktopia WHERE
+//       address='${URLParams.get('address')}'`
+//     }
+//     await getDB(query)
+//     console.log(resp)
+//     response.writeHead(200, {
+//       "Access-Control-Allow-Headers": "*",
+//       "Access-Control-Allow-Origin" : "*",
+//       'Content-Type': 'application/json'
+//     })
+//     response.end(JSON.stringify(resp));
+//   } 
+// })
 
 getDB = async(query) => {
   var conString = "postgres://ujbgxlwy:Seo5GSBlI3W7rZdgOPems-QTC6kZ9IbN@ziggy.db.elephantsql.com:5432/ujbgxlwy" //Can be found in the Details page
@@ -78,3 +188,5 @@ getDB = async(query) => {
     });
   });
 }
+
+startExpress();
